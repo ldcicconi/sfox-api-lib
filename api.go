544974e21231493104fe69cd0141b7cd568f9f72
Leonardo <ldcicconi@gmail.com>
2019-11-06 13:10:47 -0800

    handle >400 status codes

--- api.go
+++ api.go
@@ -3,9 +3,16 @@ package sfoxapi
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
+
+var ErrJsonUnmarshalling = fmt.Errorf("error unmarshalling JSON body")
+
+type SFOXErrorMsg struct {
+	Error string `json:"error"`
+}
 
 // TODO: better errors
 func (api *SFOXAPI) doRequest(action, path string, body interface{}, result interface{}) (bodyBytes []byte, statusCode int, err error) {
@@ -31,14 +38,30 @@ func (api *SFOXAPI) doRequest(action, path string, body interface{}, result inte
 	}
 	defer resp.Body.Close()
 	statusCode = resp.StatusCode
-	//read body
+	// read body
 	bodyBytes, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return
 	}
-	//try to unmarshal
+	// check status code
+	if statusCode >= 400 {
+		// try to marshal into error message
+		var errorMsg SFOXErrorMsg
+		err = json.Unmarshal(bodyBytes, errorMsg)
+		if err != nil {
+			err = ErrJsonUnmarshalling
+			return
+		} else {
+			err = fmt.Errorf("statusCode: %d error_message: %s", statusCode, errorMsg.Error)
+		}
+	}
+	// try to unmarshal
 	if result != nil {
 		err = json.Unmarshal(bodyBytes, result)
+		if err != nil {
+			err = ErrJsonUnmarshalling
+			return
+		}
 	}
 	// return
 	return
